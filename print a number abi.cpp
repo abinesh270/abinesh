c5b4db5aa37c9e6fed2ffe74f259a60d87fa4c2d
abinesh270 <113411997+abinesh270@users.noreply.github.com>
2022-09-14 14:52:27 +0530

    Add files via upload

--- print a number abi.cpp
+++ print a number abi.cpp
@@ -1,49 +1,41 @@
 #include<iostream>
 using namespace std;
-class print number
+class printnumber
 {
 	int n1;
-	char a[20];
+	char *p;
 	float n2;
 	double n3;
 	long int n4;
-public:
-	print number (int);
-	print number (char);
-	print number (float);
-	prnt number (double);
-	print number (long int);
-}
-print number::print number(int a)
+	public:
+		printnumber(int);
+		printnumber(char*);
+		printnumber(float);
+		printnumber(double);
+		printnumber(long int);   
+};
+printnumber::printnumber(int a)
 {
 	n1=a;
 	cout<<"integer is"<<a;
 }
-print number::print number(char b)
+printnumber::printnumber(char* b) 
 {
-	print number=b;
-	cout<<"char is"<<b;
+	p=b;
+	cout<<"character is"<<b;
 }
-print number::print number (float c)
+printnumber::printnumber(float c)
 {
-	print number=c;
+	n2=c;
 	cout<<"float is"<<c;
 }
-print number::print number(double d)
+printnumber::printnumber(double d)
 {
-	print number=d;
+	n3=d;
 	cout<<"double is"<<d;
 }
-print number::print number(long int e)
+int main()
 {
-	print number=e;
-	cout<<"long int is"<<e;
-}
-void main()
-{
-	print number p1(50);
-	print number p2("HELLO");
-	print number p3(0.566);
-	print number p4(3.912348239293);
-	print number p5(5366623);
+	printnumber p1(88),p2("hi"),p3(3.15),p4(5.2344),p5(12346);
+	return 0;
 }
