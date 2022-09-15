db4ce0205ca381fbab71be5f5766a93415aa3573
abinesh270 <113411997+abinesh270@users.noreply.github.com>
2022-09-15 10:06:13 +0530

    Add files via upload

---
+++ single inheritance abi.cpp
@@ -0,0 +1,28 @@
+//this is single inheritance
+#include<iostream>
+using namespace std; 
+class p
+{
+	int x=4;
+	public:
+	void getdata1()
+	{
+		cout<<"x value is"<<x;
+	}
+};
+class C:public p
+{
+	int y=5;
+	public: 
+	void getdata()
+	{
+		cout<<"y value is"<<y;
+	}
+};
+int main()
+{
+	C c;
+	c.getdata();
+	c.getdata1();
+	return 0;
+}
